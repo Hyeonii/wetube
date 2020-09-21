22dcafc9312e1df02d439a4533543aff9483d3fc
Hyeonii <63392456+Hyeonii@users.noreply.github.com>
2020-09-21 22:33:34 +0900

    USER AUTHENTICATION
    I have to proceed with Facebook user authentication later

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ import morgan from "morgan"; // to this "import" download babel
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 
+import "./passport";
+
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
+const CookieStore = MongoStore(session);
 
-// (all of them are middlewear)
-app.use(helmet()); // for Security (both of them are middlewear)
+// (all of them are middleware)
+app.use(helmet({ contentSecurityPolicy: false })); // for Security (both of them are middlewear)
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
@@ -25,9 +30,20 @@ app.use(cookieParser()); //for save cookies
 app.use(bodyParser.json()); // to get info from User
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev")); // going to log anything
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddleware);
 
-// rout
+// routes
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
